def0512d0d54332d2d74702161a22861f4f571b5
yeahnoob <zgemlbox@gmail.com>
2014-08-29 19:37:07 +0800

    Change the loop syntax, still running too slow.

--- countpairs.pl
+++ countpairs.pl
@@ -3,16 +3,18 @@ use v6;
 my $file=open 'wordpairs.txt';
 
 my %dict;
+my $line;
 
-for $file.lines -> $line {
+repeat {
+    $line=$file.get;
     my ($p1,$p2)=$line.split(' ');
     if ?%dict{$p1} {
         %dict{$p1} = "{%dict{$p1}} {$p2}".words;
     } else {
         %dict{$p1} = $p2;
     }
-}
+} while !$file.eof;
 
-say "The pairs count of the key word \"her\" in word-pairs.txt is {%dict{"her"}.elems}";
+say "The pairs count of the key word \"her\" in wordpairs.txt is {%dict{"her"}.elems}";
 
 # vim: ft=perl6
