c1cdb93288b3a7069bb0d84cbb035c45640228f5
Tobias Leich <email@froggs.de>
2014-09-03 18:57:18 +0200

    faster implementation
    This is like colomon++'s version takes from https://gist.github.com/colomon/c2d2c1940b92d8d9df01
    but improved to be about 20% faster.
    That is still slow compared to other languages, but it should improve when the MATCH deopts shown
    here are gone: http://www.harmonyware.com/files/profile-1409750803.51175.html#osrdeopt

--- countpairs.pl
+++ countpairs.pl
@@ -1,21 +1,15 @@
-use v6;
-
-my $file=open "wordpairs.txt", :r;
-
 my %dict;
-my $line;
 
-repeat {
-    $line=$file.get;
-    my ($p1,$p2)=$line.split(' ');
-    if ?%dict{$p1} {
-        %dict{$p1} = "{%dict{$p1}} {$p2}".words;
-    } else {
-        %dict{$p1} = $p2;
-    }
-} while !$file.eof;
+grammar WordPairs {
+    token word-pair { (\S*) ' ' (\S*) "\n" }
+    token TOP { <word-pair>* }
+}
 
-## Test
+class WordPairsActions {
+    method word-pair($/) { %dict{$0}.push($1) }
+}
+
+my $match = WordPairs.parse(slurp, :actions(WordPairsActions));
+say ?$match;
+ 
 say "The pairs count of the key word \"her\" in wordpairs.txt is {%dict{"her"}.elems}";
-
-# vim: ft=perl6
